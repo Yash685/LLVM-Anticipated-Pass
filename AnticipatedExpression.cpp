65b2eb9162abe29caee588fff96a2d702428be55
Yash Punjabi <57110893+Yash685@users.noreply.github.com>
2023-03-12 01:48:24 +0530

    Added Code Hoisting

--- AnticipatedExpression.cpp
+++ AnticipatedExpression.cpp
@@ -1,11 +1,15 @@
 #include "llvm/Pass.h"
 #include "llvm/ADT/BitVector.h"
+#include "llvm/ADT/DepthFirstIterator.h"
+#include "llvm/ADT/PostOrderIterator.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/CFG.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/InstIterator.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
+#include <list>
 #include <map>
 #include <set>
 #include <string>
@@ -22,17 +26,36 @@ namespace{
             Instruction* I;
             std::string opname;
             unsigned int opcode;
+            bool isHoisted;
+            Instruction *hoist_inst;
             Expression(){
                 op1 = NULL;
                 op2 = NULL;
                 opcode = -1;
                 opname = "";
                 I = NULL;
-            } 
+                isHoisted = false;
+                hoist_inst = NULL;
+            }
+             
             bool operator== (const Expression &ele) const{
-                return (this->op1 == ele.op1) && (this->op2 == ele.op2) && (this->opname == ele.opname);
+                return (this->op1 == ele.op1) && (this->op2 == ele.op2) && (this->opcode == ele.opcode);
             }
         };
+
+        bool canBeAdded(Expression *expr, Expression *ele){
+            errs() << "\n Compare Instructions: \n";
+            expr->I->print(errs()) ;
+            errs() << "\n";
+            ele->I->print(errs());
+            errs() << "\n";
+            if((expr->opcode == ele->opcode) && (expr->op1 == ele->op1) && (expr->op2 == ele->op2)){
+                errs() << "True\n";
+                return true;
+            }
+                    // return true;
+            return false;
+        }
 
         bool isFeasibleInstruction(Instruction &I)
         {
@@ -56,9 +79,9 @@ namespace{
                         
                         bool f = true;
                         for(auto *ele: expressions){
-                            if((*expr) == *(ele)){
+                            if(canBeAdded(expr, ele)){
                                 f = false;
-                                exToBlock[exToBit[expr]].insert(&BB);
+                                //exToBlock[exToBit[expr]].insert(&BB);
                                 break;
                             }
                         }
@@ -69,6 +92,7 @@ namespace{
                             exToBlock[k].insert(&BB);
                             k++;
                         }
+                        
                     }
                 }
             }
@@ -77,81 +101,190 @@ namespace{
 
         bool runOnFunction(Function& F){
             // Expression to bit
+            bool changed = true;
+            int iter = 0;
+            while(changed){
+            iter++;
             DenseMap<Expression *, unsigned> exToBit;
             DenseMap<unsigned, Expression *> BitToex;
             DenseMap<unsigned, std::set<BasicBlock *>> exToBlock;
             unsigned expressPos = 0;
 
-            // Compute Expression
-            std::set<Expression *> expressions = computeExpression(F, exToBit, BitToex, exToBlock, expressPos);
-            unsigned int size = expressions.size();
-            std::map<BasicBlock *, BitVector> UseBitMap, DefBitMap, INBitMap, OUTBitMap;
-
-            // Compute USE and DEF
-            //std::map<BasicBlock *, std::set<std::pair<Value *, std::pair<Value *, std::string>>>> useMap, defMap;
-            std::map<BasicBlock *, std::set<Expression *>> useMap, defMap;
-            useMap = computeUSE(F);
-            defMap = computeDEF(F);
-            UseBitMap = computeBIT(F, useMap, exToBit, expressions,size);
-            DefBitMap = computeBIT(F, defMap, exToBit, expressions, size);
-
-            // Initialise IN and OUT sets
-            initialiseIN_OUT(F, INBitMap, OUTBitMap, size);
-
-            //Compute IN and OUT sets
             
-            bool modified = true;
-            while(modified){
-                modified = false;
-                for(auto &BB: reverse(F)){
-                    BitVector INBit = INBitMap[&BB];
-                    BitVector OUTBit = OUTBitMap[&BB];
-                    BitVector UseBit = UseBitMap[&BB];
-                    BitVector DefBit = DefBitMap[&BB];
-                    BitVector tempBit(size, false);
-                    if(!successors(&BB).empty()){
-                        OUTBit = INBitMap[*succ_begin(&BB)];
-                        for(BasicBlock *succ: successors(&BB)){
-                            BitVector INBit = INBitMap[succ];
-                            OUTBit &= INBit;
+                changed = false;
+                // Compute Expression
+                std::set<Expression *> expressions = computeExpression(F, exToBit, BitToex, exToBlock, expressPos);
+                unsigned int size = expressions.size();
+                std::map<BasicBlock *, BitVector> UseBitMap, DefBitMap, INBitMap, OUTBitMap;
+
+                // Compute USE and DEF
+                //std::map<BasicBlock *, std::set<std::pair<Value *, std::pair<Value *, std::string>>>> useMap, defMap;
+                std::map<BasicBlock *, std::set<Expression *>> useMap, defMap;
+                useMap = computeUSE(F);
+                defMap = computeDEF(F);
+                UseBitMap = computeBIT(F, useMap, exToBit, expressions,size);
+                DefBitMap = computeBIT(F, defMap, exToBit, expressions, size);
+
+                // Initialise IN and OUT sets
+                initialiseIN_OUT(F, INBitMap, OUTBitMap, size);
+
+                //Compute IN and OUT sets
+                
+                bool modified = true;
+                while(modified){
+                    modified = false;
+                    for(auto &BB: reverse(F)){
+                        BitVector INBit = INBitMap[&BB];
+                        BitVector OUTBit = OUTBitMap[&BB];
+                        BitVector UseBit = UseBitMap[&BB];
+                        BitVector DefBit = DefBitMap[&BB];
+                        BitVector tempBit(size, false);
+                        if(!successors(&BB).empty()){
+                            OUTBit = INBitMap[*succ_begin(&BB)];
+                            for(BasicBlock *succ: successors(&BB)){
+                                BitVector INBit = INBitMap[succ];
+                                OUTBit &= INBit;
+                            }
+                        }
+                        
+                        OUTBitMap[&BB] = OUTBit;
+                        DefBit.flip();
+                        OUTBit &= DefBit;
+                        OUTBit|=UseBit;
+                        if(!(OUTBit == INBit) ){
+                            modified = true;
+                        }
+                        INBitMap[&BB] = OUTBit;
+                    }
+                }
+                printInstructions(F);
+                printMap(useMap, "USE SET", F);
+                printMap(defMap, "DEF SET", F);
+                printMapping(exToBit);
+                
+                // errs() << "IN\n";
+                // printBitMap(F, INBitMap);
+                // errs() << "OUT\n";
+                // printBitMap(F, OUTBitMap);
+                // errs() << "USE\n";
+                // printBitMap(F, UseBitMap);
+                // errs() << "DEF\n";
+                // printBitMap(F, DefBitMap);
+                errs() << "IN BIT MAP\n";
+                printBitMap(F, INBitMap, BitToex);
+                errs() << "OUT BIT MAP\n";
+                printBitMap(F, OUTBitMap, BitToex);
+                errs() << "USE BIT MAP\n";
+                printBitMap(F, UseBitMap, BitToex);
+                errs() << "DEF BIT MAP\n";
+                printBitMap(F, DefBitMap, BitToex);
+                errs() << "USE MAP\n";
+                printOGmap(F, useMap);
+                errs() << "\nDEF MAP\n";
+                printOGmap(F, defMap);
+                //printexToBlock(exToBlock);
+                //printexToBit(exToBit);
+                codeHoisting(F,exToBlock, exToBit, BitToex, OUTBitMap, INBitMap, changed);
+                if(changed)
+                    changed = true;
+            }
+            return true;
+        }
+
+
+        void codeHoisting(Function &F, DenseMap<unsigned,std::set<BasicBlock*>> &exToBlock, DenseMap<Expression *, unsigned> &exToBit, DenseMap<unsigned, Expression *> &BitToex, std::map<BasicBlock *, BitVector> &OUTBitmap, std::map<BasicBlock *, BitVector> &INBitmap, bool &changed){
+            errs() << "\nNormal Order \n";
+            for(BasicBlock &BB: F){
+                errs() << *(BB.begin()) << "-----" << &BB << "\n";
+            }
+            std::list<BasicBlock *>DFSBlocks;
+            for(BasicBlock *BB: depth_first(&F.getEntryBlock())){
+                DFSBlocks.push_back(BB);
+            }
+            
+            errs() << "\nDFS Order \n";
+            for(auto ele: DFSBlocks){
+                errs() << *ele->begin() << "-----" << ele << "\n" ;
+            }
+            bool isHoisted = false;
+            for(auto BB: DFSBlocks){
+                BitVector OUT = OUTBitmap[BB];
+                BitVector IN = INBitmap[BB];
+
+                errs() << "\n Printing OUT Set\n";
+                for(int i=0; i<OUT.size(); i++){
+                    errs() << OUT[i] << " ";
+                }
+                if(OUT.none())
+                    continue;
+                
+                errs() << "\n Checking if OUT bit is set \n";
+                for(int i=0; i<OUT.size(); i++){
+                    if(OUT[i] == 1){
+                        // Get Expression
+                        errs() << "\n INSIDE \n";
+                        Expression *hoist_expr = BitToex[i];
+                        BinaryOperator *bo = nullptr;
+                        
+                        // errs() << "New Instruction Count: \n" << c << "\n";
+                        
+                        //std::set<BasicBlock *> bblist = exToBlock[i];
+
+                        // Traverse the successors and remove this expression
+                        errs() << "Starting---- \n";
+                        std::list<BasicBlock *>DownBlocks;
+                        for(BasicBlock *item: depth_first(BB))
+                            DownBlocks.push_back(item);
+
+                        errs() << "\n DownBlocks for: " << BB << "\n";
+                        for(auto ele: DownBlocks)
+                            errs() << ele << "\n";
+                        if(DownBlocks.empty())
+                            errs() << "\nEMPTY...\n";
+                        for(BasicBlock *succ: DownBlocks)
+                        {
+                            errs() << "\n Taking :  " << *succ << " \n";
+                            //auto bbit = bblist.find(succ);
+                            //if(bbit!=bblist.end()){
+                                BasicBlock::iterator it = succ->begin();
+                                while(it != succ->end()){
+                                    //Instruction *I = &*it;
+                                    if(Instruction *I = dyn_cast<Instruction>(it)){
+                                        I->print(errs());
+                                        errs() << "\n";
+                                        it++;
+                                            
+                                            if(I->getOperand(0) == hoist_expr->op1 && I->getOperand(1) == hoist_expr->op2 && I->getOpcode() == hoist_expr->opcode){
+                                                isHoisted = true;
+                                                Instruction *newI;
+                                                if(hoist_expr->isHoisted){
+                                                   newI = hoist_expr->hoist_inst; 
+                                                }
+                                                else{
+                                                    hoist_expr->isHoisted = true;
+                                                    newI = bo->Create(static_cast<llvm::Instruction::BinaryOps>(hoist_expr->opcode), hoist_expr->op1, hoist_expr->op2, "yp", dyn_cast<Instruction>(BB->getTerminator()));
+                                                    hoist_expr->hoist_inst = newI;
+                                                }
+                                                errs() << "\n Hoisted Instruction\n";
+                                                newI->print(errs());
+                                                errs() << "\n------\n";
+                                                I->replaceAllUsesWith(newI);
+                                                //Instruction *toDelete = &*it;  // Save a pointer to the instruction to delete
+                                                //++it;  // Increment the iterator before deleting the instruction
+                                                I->eraseFromParent();  // Delete the instruction
+                                                //it = succ->begin();  
+                                            }
+                                        
+                                    }
+                                }
+                            //}
                         }
                     }
                     
-                    OUTBitMap[&BB] = OUTBit;
-                    DefBit.flip();
-                    OUTBit &= DefBit;
-                    OUTBit|=UseBit;
-                    if(!(OUTBit == INBit) ){
-                        modified = true;
-                    }
-                    INBitMap[&BB] = OUTBit;
                 }
             }
-            printMap(useMap, "USE", F);
-            printMap(defMap, "DEF", F);
-            printMapping(exToBit);
-            // errs() << "IN\n";
-            // printBitMap(F, INBitMap);
-            // errs() << "OUT\n";
-            // printBitMap(F, OUTBitMap);
-            // errs() << "USE\n";
-            // printBitMap(F, UseBitMap);
-            // errs() << "DEF\n";
-            // printBitMap(F, DefBitMap);
-            errs() << "IN\n";
-            printBitMap(F, INBitMap, BitToex);
-            errs() << "OUT\n";
-            printBitMap(F, OUTBitMap, BitToex);
-            errs() << "USE\n";
-            printBitMap(F, UseBitMap, BitToex);
-            errs() << "DEF\n";
-            printBitMap(F, DefBitMap, BitToex);
-            errs() << "USE MAP\n";
-            printOGmap(F, useMap);
-            errs() << "\nDEF MAP\n";
-            printOGmap(F, defMap);
-            printexToBlock(exToBlock);
-            return true;
+            if(isHoisted)
+                changed = true;
         }
 
         bool isSameInst(Instruction &I, Instruction &J){
@@ -171,14 +304,14 @@ namespace{
                 }
                 for (auto &use : ds[&BB]) {
                     for(auto *ele: expressions){
-                        if((*use) == *(ele)){   
+                        if(canBeAdded(use, ele)){   
                             unsigned bit = exToBit[ele];
                             helper.set(bit);
-                            break;
+                            // break;
                         }
                     }
-                    unsigned bit = exToBit[use];
-                    helper.set(bit);
+                    // unsigned bit = exToBit[use];
+                    // helper.set(bit);
                 }
                 BlockBitMap[&BB] = helper;
             }
@@ -259,8 +392,20 @@ namespace{
             }
         }
 
+        void printInstructions(Function &F){
+            for(BasicBlock &BB: F){
+                for(Instruction &I:BB){
+                    if(isFeasibleInstruction(I)){
+                        errs() << "\n";
+                        I.print(errs());
+                        errs() << I.getOpcode() << " \n";
+                    }
+                }
+            }
+        }
+
         void printMap(std::map<BasicBlock *, std::set<Expression*>> ds, std::string s, Function &F){
-            errs() << s << " \n";
+            errs() << "\n" << s << " \n";
             for (auto &BB : F) {
                 errs() << "Basic Block " << BB.getName() << ":\n";
 
@@ -284,7 +429,7 @@ namespace{
         // Print Mapping Map
         void printMapping(DenseMap<Expression *, unsigned> exToBit){
             for(const auto &pair: exToBit){
-                errs() << " " << pair.second ;
+                errs() << "Instruction:  " << *(pair.first->I) << " ------- " << "Bit: " << pair.second << "\n" ;
             }
             errs() << "\n";
         }
@@ -293,7 +438,7 @@ namespace{
         void printexToBlock(DenseMap<unsigned, std::set<BasicBlock *>> exToBlock){
             for(const auto &pair: exToBlock){
                 for(auto ele: pair.second){
-                    errs() << ele << " ";
+                    errs() << "BasicBlock:" << ele << " -----  " << "Instruction: " << *(ele->begin()) << "\n";
                 }
                 errs() << "\n";
             }
@@ -311,13 +456,14 @@ namespace{
         // }
 
         void printBitMap(Function &F, std::map<BasicBlock *, BitVector> bmap, DenseMap<unsigned, Expression*> BitToex){
+            
             for(auto &BB: F){
                 BitVector temp = bmap[&BB];
                 for (auto i = 0; i < temp.size(); i++) {
                     errs() << temp[i];
                     if(temp[i] == 1){
-                        auto ele = BitToex[i];
-                        errs() << *(ele->I);
+                        auto *ele = BitToex[i];
+                        errs() << *(ele->I) << " ";
                     }
                 }
                 errs() << "\n";
@@ -328,7 +474,7 @@ namespace{
         void printOGmap(Function &F, std::map<BasicBlock *, std::set<Expression *>> ds){
             for(auto &BB: F){
                 auto temp = ds[&BB];
-                for(auto expr: temp){
+                for(auto *expr: temp){
                     errs() << "\nOP1: " << *expr->op1;
                     errs() << "\nOP2: " << *expr->op2;
                     errs() << "\nOPName: " << expr->opname;
@@ -336,7 +482,6 @@ namespace{
             }
             errs() << "\n";
         }
-
     };
 } //namespace
 
